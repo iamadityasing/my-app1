91664c458a44c3cd999b092ec4edb4c2c89df91e
Aditya Singh <adityasingh18122003@gmail.com>
2023-09-30 11:06:35 +0530

    yes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App'
 
-var myelement=<h1> Hello World, welcome to react.....</h1>
+//var myelement=<h1> Hello World, welcome to react.....</h1>
+// function Hello(){
+//       return <h1>hello world</h1>
+// }
 const root=ReactDOM.createRoot(document.getElementById('root'));
-root.render(myelement);
+//root.render(myelement);
+root.render(<App/>);
